efc94382c18c7702b91e305169ead89760b58a40
Gabriela Agnês Custodio <Gabriela Agns Custodio>
2016-09-19 22:50:38 -0300

    merge

---
+++ IFSP.ADS.FrontEnd/IFSP.ADS.Web/app/athlete/athleteController.js
@@ -0,0 +1,15 @@
+﻿(function () {
+    'use strict';
+
+    atheleteController.module('app').controller('atheleteController', atheleteController);
+
+    atheleteController.$inject = ['$rootScope', 'categoryService'];
+
+    function atheleteController($rootScope, categoryService) {
+        var vm = this;
+
+        activate();
+
+
+    }
+})();
---
+++ IFSP.ADS.FrontEnd/IFSP.ADS.Web/app/home/homeController.js
@@ -0,0 +1,15 @@
+﻿(function () {
+    'use strict';
+
+    homeController.module('app').controller('homeController', homeController);
+
+    homeController.$inject = ['$rootScope', 'categoryService'];
+
+    function homeController($rootScope, categoryService) {
+        var vm = this;
+
+        activate();
+
+
+    }
+})();
